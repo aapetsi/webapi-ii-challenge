e6c492b507be08af8db097a4f040b07b5727379b
Apetsi Ampiah <apetsiampiah@gmail.com>
2019-10-29 12:36:48 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,13 @@
+const express = require('express');
+
+const comments = require('./routes/api/comments');
+const posts = require('./routes/api/posts');
+
+const app = express();
+
+app.use('/api/comments', comments);
+app.use('/api/posts', posts);
+
+const port = process.env.PORT || 5000;
+
+app.listen(port, () => console.log(`Server is running on port ${port}`));
